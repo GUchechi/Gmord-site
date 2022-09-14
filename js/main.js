101e8392d87195b027613361f2f086ab5dd59d49
GUchechi <godswilluchendu7@gmail.com>
2022-09-14 02:11:40 +0100

    services

--- js/main.js
+++ js/main.js
@@ -1,18 +1,34 @@
 jQuery(document).ready(function() {
-
+    // Ripple effect
     "use strict";
 
     $('.slider').ripples({
-        dropRadius: 20,
+        dropRadius: 15,
         perturbance: 0.02,
       });
+      // End of Ripple effect
 
+
+      // Animated Typed.js
       $(".text").typed({
         strings:["<strong>I love</strong><strong class='primary'> coding.</strong>","<strong>and to</strong><strong class='primary'> share !!.</strong>"],
          typespeed: 0,
-         loop:true
-         
+         loop:true         
      });
+    //  End of Typed.js
 
+    // Sticky navbar
+    $(window).scroll(function(){
+        
+      var top = $(window).scrollTop();
+       if(top>=60){
+         $("nav").addClass('secondary'); 
+       }
+       
+       else 
+           if($("nav").hasClass('secondary')){
+               $("nav").removeClass('secondary'); 
+           }
+   });
      
 });
